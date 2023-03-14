2f7c18f124c07121233bc555e3108188bcb8f104
tHeCreator <marcsabalete@gmail.com>
2023-03-15 00:46:47 +0100

    Comit2.b

--- src/main/java/org/example/Ejercicio2/Main.java
+++ src/main/java/org/example/Ejercicio2/Main.java
@@ -1,4 +1,8 @@
 package org.example.Ejercicio2;
 
 public class Main {
+    public static void main(String[] args) {
+        MostrarMain mostrar = new MostrarMain();
+        mostrar.mostrarDatos();
+    }
 }
--- src/main/java/org/example/Ejercicio2/MostrarMain.java
+++ src/main/java/org/example/Ejercicio2/MostrarMain.java
@@ -1,7 +1,42 @@
 package org.example.Ejercicio2;
-
+import java.util.Scanner;
 public class MostrarMain {
     public void mostrarDatos(){
+
+                double distancia, tiempo, velocidad;
+                String idioma;
+
+                Scanner sc = new Scanner(System.in);
+
+                System.out.println("Seleccione el idioma / Select language:");
+                System.out.println("1. Español");
+                System.out.println("2. English");
+                System.out.print("Opción / Option: ");
+                idioma = sc.nextLine();
+
+                Traduccion traduccion;
+
+                if (idioma.equals("1")) {
+                    traduccion = new TraductorEspanol();
+                } else {
+                    traduccion = new TraductorIngles();
+                }
+
+                traduccion.introducirDistancia();
+                distancia = sc.nextDouble();
+
+                traduccion.introducirTiempo();
+                tiempo = sc.nextDouble();
+
+
+                tiempo = tiempo / 60;
+
+
+                velocidad = distancia / tiempo;
+
+                traduccion.inicioRespuesta();
+                System.out.print(velocidad);
+                traduccion.finRespuesta();
 
     }
 }
