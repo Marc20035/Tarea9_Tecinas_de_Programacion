d251071e873a30e9feb1ecf002a530a6a812c18f
tHeCreator <marcsabalete@gmail.com>
2023-03-15 00:10:25 +0100

    Comit1.c

--- src/main/java/org/example/Ejercicio1/Ciclos.java
+++ src/main/java/org/example/Ejercicio1/Ciclos.java
@@ -24,37 +24,6 @@ public abstract class Ciclos {
         this.precio = precio;
     }
 
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
-
-    public LocalDate getFechaCompra() {
-        return fechaCompra;
-    }
-
-    public void setFechaCompra(LocalDate fechaCompra) {
-        this.fechaCompra = fechaCompra;
-    }
-
-    public double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(double precio) {
-        this.precio = precio;
-    }
 
 
 }
